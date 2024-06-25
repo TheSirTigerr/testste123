f73f59681df6d8476e22e4b8982c51a3879bf8f7
TheSirTigerr <96537622+TheSirTigerr@users.noreply.github.com>
2024-06-25 11:06:59 +0200

    Delete mcsleepingserverstarter directory

--- mcsleepingserverstarter/src/sleepingContainer.test.ts
+++
@@ -1,9 +0,0 @@
-import { SleepingContainer } from "./sleepingContainer";
-import { DefaultSettings } from "./sleepingSettings";
-
-test("should start the container with default settings", (done) => {
-  new SleepingContainer((settings) => {
-    expect(settings).toEqual(DefaultSettings);
-    done();
-  });
-});
